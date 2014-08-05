7c40a3b7dee69ccd4358ffce7a7e65e4be2b676a
Ben <twelve1six@naver.com>
2014-08-05 17:59:38 +0900

    Fix layout.

---
+++ P_head_v.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+    <html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
+		<style type="text/css">
+			body {
+				font-size: 12px;
+			}
+			ul {
+				padding-left: 10px;
+			}
+			li {
+				list-style: none;
+				padding-top: 5px;				
+			}
+			.wraper {
+				width: 1156px;
+			}
+			.logo {
+				float: left;
+				width: 120px;
+				height: 60px;
+				border: 1px solid blue;
+				margin: 0 auto
+			}
+			.login {
+				float: left;
+				width: 100px;
+				height: 60px;
+				border: 1px solid blue;
+				margin: 0 auto
+			}
+			.ReportSearch {
+				float: left;
+				width: 90px;
+				height: 60px;
+				border: 1px solid blue;
+				overflow-y:hidden;
+				margin: 0 auto
+			}
+			.PathologyReport {
+				float: left;
+				width: 90px;
+				height: 60px;
+				border: 1px solid blue;
+				overflow-y:hidden;
+				margin: 0 auto
+			}
+			.ImageSearch {
+				float: left;
+				width: 90px;
+				height: 60px;
+				border: 1px solid blue;
+				overflow-y:hidden;
+				margin: 0 auto
+			}
+			.PrivateLibrary {
+				float: left;
+				width: 90px;
+				height: 60px;
+				border: 1px solid blue;
+				overflow-y:hidden;
+				margin: 0 auto
+			}
+			.PublicLibrary {
+				float: left;
+				width: 90px;
+				height: 60px;
+				border: 1px solid blue;
+				overflow-y:hidden;
+				margin: 0 auto
+			}
+			.Messenger {
+				float: left;
+				width: 90px;
+				height: 60px;
+				border: 1px solid blue;
+				overflow-y:hidden;
+				margin: 0 auto
+			}
+			.SendReport {
+				float: left;
+				width: 90px;
+				height: 60px;
+				border: 1px solid blue;
+				overflow-y:hidden;
+				margin: 0 auto
+			}
+			.Banner {
+				float: left;
+				width: 1000px;
+				height: 30px;
+				border: 1px solid blue;
+				margin: 0 auto
+			}
+			.SearchFilter {
+				float: left;
+				width: 500px;
+				height: 200px;
+				border: 1px solid blue;
+				margin: 0 auto
+			}
+			.Category {
+				float: left;
+				width: 500px;
+				height: 400px;
+				border: 1px solid blue;
+				margin: 0 auto
+			}
+			.PathologyResult {
+				clear: both;
+				float: left;
+				width: 500px;
+				height: 200px;
+				border: 1px solid blue;
+				margin: 0 auto
+			}	
+			.footer {
+				clear: both;
+				border: 1px solid purple;
+				height: 20px;
+			}
+		</style>
+	</head>
+	<body>
