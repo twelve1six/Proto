d711694ee170fb8338f2e966dd993b481f9a8794
Ben <twelve1six@naver.com>
2014-08-18 17:33:21 +0900

    fix layout and making reportsearch.(have to make gallery)

--- view/PB_pathologyreport_v.php
+++ view/PB_pathologyreport_v.php
@@ -88,13 +88,12 @@ if($this->session->userdata('is_login')) {
 				<span class="label label-primary">Reception Number</span>
 				<div class="form-group form-inline">
 					<div class="col-md-4">
-						<input type="text" class="form-control" id="pathologyNum" placeholder="Pathology Number">
+						<input type="text" class="form-control" id="receptionNum" placeholder="Reception Number">
 					</div>
 					<button type="submit" class="btn btn-primary">
 						PACS
 					</button>
 				</div>
-
 				<div class="form-group col-md-4">
 					<table class="table table-bordered table-hover table-condensed">
 						<tr>
@@ -103,18 +102,17 @@ if($this->session->userdata('is_login')) {
 							<th>Gender</th>
 							<th>Department</th>
 						</tr>
+						<tr>
+							<?php $name = '권봉귀';
+							$age = 43;
+							$gender = 'M';
+							$department = 'ORC';
 
-						<?php
-						$name = '권봉귀';
-						$age = 43;
-						$gender = 'M';
-						$department = 'ORC';
-
-						echo '<td>' . $name . '</td>';
-						echo '<td>' . $age . '</td>';
-						echo '<td>' . $gender . '</td>';
-						echo '<td>' . $department . '</td>';
-						?>
+							echo '<td>' . $name . '</td>';
+							echo '<td>' . $age . '</td>';
+							echo '<td>' . $gender . '</td>';
+							echo '<td>' . $department . '</td>';
+							?>
 						</tr>
 					</table>
 				</div>
@@ -122,239 +120,130 @@ if($this->session->userdata('is_login')) {
 				<div class="row">
 					&nbsp;
 				</div>
-
-				<div class="filter_title list_title">
+				<div class="filter_title">
 					List</br>
 				</div>
-				<!-- <div class="form-group form-inline">
-				<div class="col-md-1">
-				<button type="submit" class="btn btn-primary">
-				Search
-				</button>
-				</div>
-				<div class="col-md-2 search_in_result">
-				<button type="submit" class="btn btn-primary">
-				Search In Results
-				</button>
-				</div>
-				<div class="col-md-1 clear">
-				<button type="reset" class="btn btn-primary">
-				Clear
-				</button>
-				</div>
-				</div> -->
-			</form>
+				<div class="form-group col-md-4">
+					<table class="table table-bordered table-hover table-condensed">
+						<tr>
+							<!-- <th>Select</th> -->
+							<th>Pathology&nbsp;Number</th>
+							<th>Reception&nbsp;Date</th>
+							<th>Diagnosis</th>
+							<th>Data</th>
+						</tr>
+						<tr>
+							<?php $Pnum = 'F 13-0000041';
+							$Rdate = '2013-04-04';
+							$Diagnosis = 'M54.21 ';
+							$Data = 3;
 
+							echo '<td>' . $Pnum . '</td>';
+							echo '<td>' . $Rdate . '</td>';
+							echo '<td>' . $Diagnosis . '</td>';
+							echo '<td>' . $Data . '</td>';
+							?>
+						</tr>
+						<tr>
+							<?php $Pnum = 'F 13-0000041';
+							$Rdate = '2013-04-04';
+							$Diagnosis = 'M54.21 ';
+							$Data = 3;
+
+							echo '<td>' . $Pnum . '</td>';
+							echo '<td>' . $Rdate . '</td>';
+							echo '<td>' . $Diagnosis . '</td>';
+							echo '<td>' . $Data . '</td>';
+							?>
+						</tr>
+						<tr>
+							<?php $Pnum = 'F 13-0000041';
+							$Rdate = '2013-04-04';
+							$Diagnosis = 'M54.21 ';
+							$Data = 3;
+
+							echo '<td>' . $Pnum . '</td>';
+							echo '<td>' . $Rdate . '</td>';
+							echo '<td>' . $Diagnosis . '</td>';
+							echo '<td>' . $Data . '</td>';
+							?>
+						</tr>
+						<tr>
+							<?php $Pnum = 'F 13-0000041';
+							$Rdate = '2013-04-04';
+							$Diagnosis = 'M54.21 ';
+							$Data = 3;
+
+							echo '<td>' . $Pnum . '</td>';
+							echo '<td>' . $Rdate . '</td>';
+							echo '<td>' . $Diagnosis . '</td>';
+							echo '<td>' . $Data . '</td>';
+							?>
+						</tr>
+						<tr>
+							<?php
+							$Pnum = 'F 13-0000041';
+							$Rdate = '2013-04-04';
+							$Diagnosis = 'M54.21 ';
+							$Data = 3;
+
+							echo '<td>' . $Pnum . '</td>';
+							echo '<td>' . $Rdate . '</td>';
+							echo '<td>' . $Diagnosis . '</td>';
+							echo '<td>' . $Data . '</td>';
+							?>
+						</tr>
+					</table>
+			</form>
+		</div>
+
+	</div>
+	<div class="btn-group library_buttons library_buttons2">
+		<button type="button" class="btn btn-primary">
+			Public Library
+		</button>
+		<button type="button" class="btn btn-primary">
+			Private Library
+		</button>
+	</div>
+	</div>
+
+	<div class="col-md-4 pathology_resultM">
+		<div class="pathology_result_title">
+			Pathologic Result</br>
+		</div>
+		<div class="row">
+			&nbsp;
+		</div>
+		<p class="text-left">
+		<?php 
+			$report = '<병리검사 의뢰서 입력내용>
+			1. 채취부위:
+			2. 수술명: 
+			3. 임상진단:
+			
+			<동결절편 진단>
+			
+			#1. 동결절편 조직은 6.0 * 1.0 * 0.5cm크기의 조직임.
+				동결절편 진단 (#1) : Proximal margin : Presence of carcinoma /HJP/
+			#2. 동결절편 조직은 1.2 * 1.0 * 0.5cm 크기의 조직임.
+				동결절편 진단 (#2) : Proximal margin : Free of carcinoma /HJP/
+				
+			<육안소견>
+			받은 조직은 모두 3부분임.
+			#1. 신성 상태의 전 절제된 위장으로 구성된 위전 절제술 검체임. 검체는 두부분으로 나뉘어져 suture 되어 연결되어 왔음. 대만곡을 따라 열려져 왔으며 위장의 크기는 대만곡을 따라 22.0cm, 소만곡을 따라 14.0cm임. 연결된 십이지장의 길이는 1.5cm, 평균 내경 5.0cm임. 점막면에 전정부, 대만곡을 중ㅅ미으로 궤양 침은형의 종	괴가 관찰됨. 종괴의 크기는 8.0cm * 8.0cm 이며 원위 절면으로부터 2.5cm, 근위 절면으로부터 6.0cm 떨어져 있음.';			
+				
+			//line feed	
+			$report = nl2br($report);
+			//change 'tap' to 'space'
+			//$report = str_replace("	", "&nbsp; &nbsp; &nbsp; &nbsp; ", $report);
+			echo $report;
+		?>
+		</p>
+	</div>
+	
+	<div class="col-md-4">
+		<div class="pathology_result_title">
+			Images</br>
 		</div>
 	</div>
-
-	<!-- <div class="col-md-6 categoryM">
-	<div class="filter_title">
-	Category</br>
-	</div>
-
-	<div class ="btn-toolbar categoryB" select multiple="multiple">
-	<div class="btn-group" select multiple="multiple">
-	<button type="button" class = "btn btn-primary">
-	ALL
-	</button>
-	<button type="button" class = "btn btn-primary">
-	S
-	</button>
-	<button type="button" class = "btn btn-primary">
-	C
-	</button>
-	</div>
-	<div class="btn-group">
-	<button type="button" class = "btn btn-primary">
-	P
-	</button>
-	<button type="button" class = "btn btn-primary">
-	AC
-	</button>
-	<button type="button" class = "btn btn-primary">
-	PC
-	</button>
-	</div>
-	<div class="btn-group">
-	<button type="button" class = "btn btn-primary">
-	PA
-	</button>
-	<button type="button" class = "btn btn-primary">
-	M
-	</button>
-	<button type="button" class = "btn btn-primary">
-	IF
-	</button>
-	<button type="button" class = "btn btn-primary">
-	E
-	</button>
-	<button type="button" class = "btn btn-primary">
-	RC
-	</button>
-	<button type="button" class = "btn btn-primary">
-	CA
-	</button>
-	</div>
-	</div>
-	<table class="table table-bordered table-hover">
-	<tr>
-	<th>Pathology&nbsp;Number</th>
-	<th>G</th>
-	<th>M</th>
-	<th>V</th>
-	<th>Register&nbsp;Number</th>
-	<th>&nbsp;Name&nbsp;</th>
-	<th>Gender</th>
-	<th>Age</th>
-	<th>&nbsp;Birth&nbsp;Date&nbsp;</th>
-	<th>Reception&nbsp;Date</th>
-	<th>Result&nbsp;Date</th>
-	<th>&nbsp;&nbsp;Doctor&nbsp;&nbsp;</th>
-	</tr>
-
-	<?php
-	//translate mark and space of string for html
-	$msg = '<동결절편> 동결절편 조직은 1.0*0.6*0.5cm and 1.2 * 0.6 * 0.5cm 크기의 림프절 2개임.';
-	$msg = htmlspecialchars($msg, ENT_NOQUOTES);
-	$tooltip = str_replace(" ", "&nbsp;", $msg);
-	echo '<tr rel="tooltip" data-placement="right" data-original-title=' . $tooltip . ' data-container="body">';
-	?>
-	<td>F 13-0000041</td>
-	<td>5</td>
-	<td>1</td>
-	<td>0</td>
-	<td>14761861</td>
-	<td>장필순</td>
-	<td>F</td>
-	<td>59</td>
-	<td>1953-09-02</td>
-	<td>2013-04-04 16:07</td>
-	<td>2013-04-12 14:18</td>
-	<td>찰리/유먼</td>
-	</tr>
-
-	<?php $tooltip = 'blahblah';
-	echo '<tr rel="tooltip" data-placement="right" data-original-title=' . $tooltip . ' data-container="body">';
-	?>
-	<td>G 13-0000041</td>
-	<td>5</td>
-	<td>1</td>
-	<td>0</td>
-	<td>14761861</td>
-	<td>이이언</td>
-	<td>F</td>
-	<td>59</td>
-	<td>1953-09-02</td>
-	<td>2013-04-04 16:07</td>
-	<td>2013-04-12 14:18</td>
-	<td>찰리</td>
-
-	</tr>
-	<?php $tooltip = 'blahblah';
-	echo '<tr rel="tooltip" data-placement="right" data-original-title=' . $tooltip . ' data-container="body">';
-	?>
-	<td>G 13-0000041</td>
-	<td>5</td>
-	<td>1</td>
-	<td>0</td>
-	<td>14761861</td>
-	<td>이이언</td>
-	<td>F</td>
-	<td>59</td>
-	<td>1953-09-02</td>
-	<td>2013-04-04 16:07</td>
-	<td>2013-04-12 14:18</td>
-	<td>찰리</td>
-
-	</tr>
-	<?php $tooltip = 'blahblah';
-	echo '<tr rel="tooltip" data-placement="right" data-original-title=' . $tooltip . ' data-container="body">';
-	?>
-	<td>G 13-0000041</td>
-	<td>5</td>
-	<td>1</td>
-	<td>0</td>
-	<td>14761861</td>
-	<td>이이언</td>
-	<td>F</td>
-	<td>59</td>
-	<td>1953-09-02</td>
-	<td>2013-04-04 16:07</td>
-	<td>2013-04-12 14:18</td>
-	<td>찰리</td>
-
-	</tr>
-	<?php $tooltip = 'blahblah';
-	echo '<tr rel="tooltip" data-placement="right" data-original-title=' . $tooltip . ' data-container="body">';
-	?>
-	<td>G 13-0000041</td>
-	<td>5</td>
-	<td>1</td>
-	<td>0</td>
-	<td>14761861</td>
-	<td>이이언</td>
-	<td>F</td>
-	<td>59</td>
-	<td>1953-09-02</td>
-	<td>2013-04-04 16:07</td>
-	<td>2013-04-12 14:18</td>
-	<td>찰리</td>
-
-	</tr>
-	<?php $tooltip = 'blahblah';
-	echo '<tr rel="tooltip" data-placement="right" data-original-title=' . $tooltip . ' data-container="body">';
-	?>
-	<td>G 13-0000041</td>
-	<td>5</td>
-	<td>1</td>
-	<td>0</td>
-	<td>14761861</td>
-	<td>이이언</td>
-	<td>F</td>
-	<td>59</td>
-	<td>1953-09-02</td>
-	<td>2013-04-04 16:07</td>
-	<td>2013-04-12 14:18</td>
-	<td>찰리</td>
-
-	</tr>
-	<?php $tooltip = 'blahblah';
-	echo '<tr rel="tooltip" data-placement="right" data-original-title=' . $tooltip . ' data-container="body">';
-	?>
-	<td>G 13-0000041</td>
-	<td>5</td>
-	<td>1</td>
-	<td>0</td>
-	<td>14761861</td>
-	<td>이이언</td>
-	<td>F</td>
-	<td>59</td>
-	<td>1953-09-02</td>
-	<td>2013-04-04 16:07</td>
-	<td>2013-04-12 14:18</td>
-	<td>찰리</td>
-
-	</tr>
-	<?php
-	$tooltip = 'blahblah';
-	echo '<tr rel="tooltip" data-placement="right" data-original-title=' . $tooltip . ' data-container="body">';
-	?>
-	<td>G 13-0000041</td>
-	<td>5</td>
-	<td>1</td>
-	<td>0</td>
-	<td>14761861</td>
-	<td>이이언</td>
-	<td>F</td>
-	<td>59</td>
-	<td>1953-09-02</td>
-	<td>2013-04-04 16:07</td>
-	<td>2013-04-12 14:18</td>
-	<td>찰리</td>
-
-	</tr>
-
-	</table>
-	</div> -->
-
